1b4be2f76f8975274440b82f56091da75e0427ec
i-walker <46971368+i-walker@users.noreply.github.com>
2019-08-21 18:15:55 +0200

    merged with rr-typeclass

--- compiler-plugin/src/main/kotlin/arrow/meta/autofold/AutoFoldPlugin.kt
+++ compiler-plugin/src/main/kotlin/arrow/meta/autofold/AutoFoldPlugin.kt
@@ -18,9 +18,12 @@ val MetaComponentRegistrar.autoFold: List<ExtensionPhase>
     meta(
       classOrObject(::isAutoFoldable) { c ->
         println("Processing Sealed class: ${c.name}")
-        val returnType = typeParameters.invariant.last().inc() // TODO: Needs to be extended to whole words for cases where TypeVariables look like this: AP, FF
         sealedVariants.forEach(::println)
-        val typeInfo = Name.identifier(c.renderTypeParameters()).identifier
+        val sealedExtraTypes = sealedVariants.map { it.typeVariables }.filter(String::isNotEmpty)
+        val typeInfo = (c.renderTypeParameters().split(", ") + sealedExtraTypes).distinct().sorted()
+        val typeInfoString = typeInfo.joinToString(separator = ", ")
+        val returnType = typeInfo.returnType
+        println(returnType)
         listOfNotNull(
           if (sealedVariants.any { it.typeVariables.split(',').size > c.arity })
           // TODO: bail here or tell User this sealed Class can't have a fold function
@@ -33,9 +36,10 @@ val MetaComponentRegistrar.autoFold: List<ExtensionPhase>
             """
               |$visibility $modality $kind $name<$typeParameters>($valueParameters)${supertypes.identifier.doIf(String::isNotEmpty) { " : $it" }} {
               |  ${body.asString().trimMargin()}
-              |  inline fun <${typeInfo.doIf(String::isNotEmpty) { "$it, " }}$returnType> ${c.name}${typeInfo.doIf(String::isNotEmpty) { "<$it>" }}.fold(
+              |  @Suppress("UNCHECKED_CAST")
+              |  fun <${typeInfoString.doIf(String::isNotEmpty) { "$it, " }}$returnType> ${c.name}${c.renderTypeParameters().doIf(String::isNotEmpty) { "<$it>" }}.fold(
               |  ${sealedVariants.params(returnType)}
-              |  ): $returnType = when(val x = this) {
+              |  ): $returnType = when (val x = this) {
               |  ${sealedVariants.patternMatch()}
               |  }
               |}
@@ -57,6 +61,11 @@ data class SealedSubclass(val simpleName: Name, val fqName: FqName?, val typeVar
 fun KtClass.sealedSubclasses(): List<SealedSubclass> =
   innerSealedSubclasses() + outerSealedSubclasses()
 
+val List<String>.returnType
+  get() = sorted().run {
+    last().dropLast(1) + with(last().last()) { if (inc().isLetterOrDigit()) inc().toUpperCase() else plus("A") }
+  }
+
 fun List<KtDeclaration>.sealedVariants(superKt: KtClass): List<SealedSubclass> =
   filter {
     (it is KtClassOrObject) && it.getSuperNames().contains(superKt.nameAsSafeName.identifier)
@@ -77,16 +86,17 @@ fun KtClass.outerSealedSubclasses(): List<SealedSubclass> =
 private fun KtClass.renderTypeParameters(): String =
   Name.identifier(typeParameters.joinToString(separator = ", ") { it.text }).invariant
 
-
 inline fun <T> T.doIf(cond: T.() -> Boolean, doIt: (T) -> T): T =
   if (cond(this)) doIt(this) else this
 
 private fun List<SealedSubclass>.patternMatch(): String = // works
   joinToString(
-    transform = { "  is ${it.simpleName.identifier} -> ${it.simpleName.identifier.decapitalize()}(x)" },
+    transform = { s ->
+      "  is ${s.simpleName.identifier} -> ${s.simpleName.identifier.decapitalize()}(x${s.typeVariables.doIf(String::isNotEmpty) { " as ${s.simpleName.identifier}<$it>" }})"
+    },
     separator = "\n  ")
 
-private fun List<SealedSubclass>.params(returns: Char): String =
+private fun List<SealedSubclass>.params(returns: String): String =
   joinToString(
-    transform = { "  crossinline ${it.simpleName.identifier.decapitalize()}: (${it.simpleName.identifier}${it.typeVariables.doIf(String::isNotEmpty) { "<$it>" }}) -> $returns" }
+    transform = { "  ${it.simpleName.identifier.decapitalize()}: (${it.simpleName.identifier}${it.typeVariables.doIf(String::isNotEmpty) { "<$it>" }}) -> $returns" }
     , separator = ",\n  ")
--- consumer/src/main/kotlin/consumer/FooClass.kt
+++ consumer/src/main/kotlin/consumer/FooClass.kt
@@ -1,7 +1,9 @@
 package consumer
 
+import arrow.not
 import arrow.Kind
 import arrow.`*`
+import arrow.extension
 
 /** HigherKinds **/
 /** AutoFold **/
@@ -13,6 +15,12 @@ sealed class Option<out A> {
     when (this) {
       None -> None
       is Some -> Some(f(value))
+    }
+
+  fun <B> flatMap(f: (A) -> Option<B>): Option<B> =
+    when (this) {
+      None -> None
+      is Some -> f(value)
     }
 }
 
@@ -27,25 +35,36 @@ class Kleisli<out F, out D, out A>
 
 interface Functor<F> {
   fun <A, B> Kind<F, A>.map(f: (A) -> B): Kind<F, B>
+  fun <A, B> Kind<F, A>.flatMap(f: (A) -> Kind<F, B>): Kind<F, B>
 }
 
-//@extension
-//class OptionFunctor : Functor<ForOption> {
+//@extension fun functorForOptionFun(): Functor<ForOption> = object : Functor<ForOption> {
 //  override fun <A, B> OptionOf<A>.map(f: (A) -> B): Option<B> =
-//    TODO()
+//    (this as Option<A>).map(f)
+//}
+//
+//@extension val FunctorForOptionVal: Functor<ForOption> = object : Functor<ForOption> {
+//  override fun <A, B> OptionOf<A>.map(f: (A) -> B): Option<B> =
+//    (this as Option<A>).map(f)
+//}
+//
+//@extension class FunctorForOptionClass: Functor<ForOption> {
+//  override fun <A, B> OptionOf<A>.map(f: (A) -> B): Option<B> =
+//    (this as Option<A>).map(f)
 //}
 
-sealed class Expr<A> {
-  fun done() = 3
-}
+sealed class Expr<out A, B>
+data class Const(val number: Double) : Expr<Int, Int>()
+data class Sum<C>(val e1: Expr<Int, C>, val e2: Expr<Int, C>) : Expr<Int, C>()
+object NotANumber : Expr<Nothing, Nothing>()
 
-data class Const(val number: Double) : Expr<Int>()
-data class Sum(val e1: Expr<Int>, val e2: Expr<Int>) : Expr<Int>() // TODO: add parametertypes of subtypes to fold
-object NotANumber : Expr<Nothing>()
-/*
-fun functorForOption(): Functor<ForOption> = object : Functor<ForOption> {
+@extension
+object FunctorForOption : Functor<ForOption> {
   override fun <A, B> OptionOf<A>.map(f: (A) -> B): Option<B> =
     (this as Option<A>).map(f)
+
+  override fun <A, B> OptionOf<A>.flatMap(f: (A) -> OptionOf<B>): Option<B> =
+    (this as Option<A>).flatMap(f)
 }
 
 fun <F> Kind<F, Int>.addOne(FF: Functor<F> = `*`): Kind<F, Int> =
@@ -56,5 +75,53 @@ class Service {
     map { it + 1 }
 }
 
-fun testConversion(): Any =
-  Option.Some(1).addOne()*/
+fun testConversionSimple(): Option<Int> =
+  Option.Some(!Option.Some(!Option.Some(!Option.Some(1))))
+
+fun testConversionInfix(): Option<Int> =
+  Option.Some(!Option.Some(1) + !Option.Some(1))
+
+/**
+ BLOCK_BODY
+  VAR name:x type:kotlin.Int [val]
+    CALL 'public final fun not <A> (): A of arrow.not declared in arrow' type=kotlin.Int origin=EXCL
+      <A>: kotlin.Int
+      $receiver: CONSTRUCTOR_CALL 'public constructor <init> (value: A of consumer.Option.Some) [primary] declared in consumer.Option.Some' type=consumer.Option.Some<kotlin.Int> origin=null
+        <class: A>: kotlin.Int
+        value: CONST Int type=kotlin.Int value=1
+  RETURN type=kotlin.Nothing from='public final fun testConversion (): consumer.Option<kotlin.Int> declared in consumer'
+    CONSTRUCTOR_CALL 'public constructor <init> (value: A of consumer.Option.Some) [primary] declared in consumer.Option.Some' type=consumer.Option.Some<kotlin.Int> origin=null
+      <class: A>: kotlin.Int
+      value: GET_VAR 'val x: kotlin.Int [val] declared in consumer.testConversion' type=kotlin.Int origin=null
+ */
+fun testConversion(): Option<Int> {
+  val x = !Option.Some(1)
+  return Option.Some(x)
+}
+
+/*
+BLOCK_BODY
+  RETURN type=kotlin.Nothing from='public final fun testConversionFlatMap (): consumer.Option<kotlin.Int> declared in consumer'
+    CALL 'public final fun flatMap <B> (f: kotlin.Function1<A of consumer.Option.Some, consumer.Option<B of consumer.Option.Some.flatMap>>): consumer.Option<B of consumer.Option.Some.flatMap> declared in consumer.Option.Some' type=consumer.Option<kotlin.Int> origin=null
+      <B>: kotlin.Int
+      $this: CONSTRUCTOR_CALL 'public constructor <init> (value: A of consumer.Option.Some) [primary] declared in consumer.Option.Some' type=consumer.Option.Some<kotlin.Int> origin=null
+        <class: A>: kotlin.Int
+        value: CONST Int type=kotlin.Int value=1
+      f: BLOCK type=kotlin.Function1<kotlin.Int, consumer.Option.Some<kotlin.Int>> origin=LAMBDA
+        FUN LOCAL_FUNCTION_FOR_LAMBDA name:<anonymous> visibility:local modality:FINAL <> (x:kotlin.Int) returnType:consumer.Option.Some<kotlin.Int>
+          VALUE_PARAMETER name:x index:0 type:kotlin.Int
+          BLOCK_BODY
+            VAR name:x type:kotlin.Int [val]
+              GET_VAR 'x: kotlin.Int declared in consumer.testConversionFlatMap.<anonymous>' type=kotlin.Int origin=null
+            RETURN type=kotlin.Nothing from='local final fun <anonymous> (x: kotlin.Int): consumer.Option.Some<kotlin.Int> declared in consumer.testConversionFlatMap'
+              CONSTRUCTOR_CALL 'public constructor <init> (value: A of consumer.Option.Some) [primary] declared in consumer.Option.Some' type=consumer.Option.Some<kotlin.Int> origin=null
+                <class: A>: kotlin.Int
+                value: GET_VAR 'val x: kotlin.Int [val] declared in consumer.testConversionFlatMap.<anonymous>' type=kotlin.Int origin=null
+        FUNCTION_REFERENCE 'local final fun <anonymous> (x: kotlin.Int): consumer.Option.Some<kotlin.Int> declared in consumer.testConversionFlatMap' type=kotlin.Function1<kotlin.Int, consumer.Option.Some<kotlin.Int>> origin=LAMBDA
+ */
+fun testConversionFlatMap(): Option<Int> =
+  Option.Some(1).flatMap { x ->
+    val x = x
+    Option.Some(x)
+  }
+
